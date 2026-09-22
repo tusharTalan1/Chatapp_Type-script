292b3257c82ca34b3725e6b3b9a80186b7ce6f7e
tusharTalan1 <tushartalan10@gmail.com>
2026-09-22 16:09:00 +0530

    feat: add schema for user and messages

--- chat-app-v2/server/src/models/Message.ts
+++ chat-app-v2/server/src/models/Message.ts
@@ -1,1 +1,29 @@
+import mongoose, { Document, Schema } from 'mongoose';
 
+export interface IMessage extends Document {
+  room: string;
+  sender: string;
+  content: string;
+  createdAt: Date;
+}
+
+const messageSchema: Schema = new mongoose.Schema({
+  room: {
+    type: String,
+    required: true
+  },
+  sender: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model<IMessage>('Message', messageSchema);

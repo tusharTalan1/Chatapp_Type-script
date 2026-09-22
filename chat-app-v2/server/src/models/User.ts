d917892f54448944f737edd6e97beeea4dbf5412
tusharTalan1 <tushartalan10@gmail.com>
2026-09-22 16:03:29 +0530

    add node modules and setup tyscript

---
+++ chat-app-v2/server/src/models/User.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+}
+
+const userSchema: Schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model<IUser>('User', userSchema);

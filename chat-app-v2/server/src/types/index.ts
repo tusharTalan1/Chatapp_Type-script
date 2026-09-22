d5f6ef31971db925ed1c909e2e5c4b09c2aeeb60
tusharTalan1 <tushartalan10@gmail.com>
2026-09-22 16:07:05 +0530

    feat: add type script interfaces

--- chat-app-v2/server/src/types/index.ts
+++ chat-app-v2/server/src/types/index.ts
@@ -1,1 +1,20 @@
+import { JwtPayload } from 'jsonwebtoken';
+export interface IJwtPayload extends JwtPayload{
+  userId: string;
+}
+export interface ServerToClientEvents{
+  message: (data: { sender: string; content: string; createdAt: Date }) => void;
+  error: (msg: string) => void;
+}
+export interface ClientToServerEvents{
+  joinRoom: (room: string) => void;
+  sendMessage: (data: { room: string; content: string }) => void;
+}
 
+export interface InterServerEvents{
+  ping: () => void;
+}
+
+export interface SocketData{
+  userId: string;
+}
